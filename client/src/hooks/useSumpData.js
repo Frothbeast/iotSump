c83f42fd4d03ab7745fae2cee0313943e5cb8cb6
Frothbeast <frothbeast@gmail.com>
2026-02-15 09:21:56 -0500

    Modified useSumpData to be less agressive and more scalable
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- client/src/hooks/useSumpData.js
+++ client/src/hooks/useSumpData.js
@@ -5,7 +5,9 @@ export function useSumpData(hours) {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        const interval = setInterval(() => {
+        let interval;
+
+        const fetchData = () => {
             fetch(`/api/sumpData?hours=${hours}`)
                 .then(res => res.json())
                 .then(data => {
@@ -18,9 +20,29 @@ export function useSumpData(hours) {
                     console.error("Fetch error:", err);
                     setIsLoading(false);
                 });
-        }, 1000);
+        };
 
-        return () => clearInterval(interval);
+        const setupInterval = () => {
+            if (interval) clearInterval(interval);
+            
+            // If tab is visible, 1 second. If hidden, 60 seconds.
+            const pollRate = document.visibilityState === 'visible' ? 1000 : 60000;
+            
+            interval = setInterval(fetchData, pollRate);
+        };
+        fetchData();
+        setupInterval();
+        const handleVisibility = () => {
+            console.log(`Visibility changed to: ${document.visibilityState}. Adjusting poll rate.`);
+            setupInterval();
+        };
+
+        document.addEventListener("visibilitychange", handleVisibility);
+
+        return () => {
+            clearInterval(interval);
+            document.removeEventListener("visibilitychange", handleVisibility);
+        };
     }, [hours]);
 
     return { sumpRecords, isLoading };
