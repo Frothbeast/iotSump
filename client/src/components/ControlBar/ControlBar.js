b546d5f7326414ce584cd6b6e88cfada0696ab54
frothbeast <frothbeast@gmail.com>
2026-03-06 14:04:58 -0500

    NOT  // Current code <button onClick="cl1pClick" className="cl1pButton">CL1P</button>
    USE
    // Corrected code
    <button onClick={cl1pClick} className="cl1pButton">CL1P</button>
    
    Signed-off-by: frothbeast <frothbeast@gmail.com>

--- client/src/components/ControlBar/ControlBar.js
+++ client/src/components/ControlBar/ControlBar.js
@@ -57,7 +57,7 @@ const ControlBar = ({ cl1pClick, selectedHours, onHoursChange, columnStats, sump
           <button className="sidebarButton" onClick={toggleSidebar}>
             {isSidebarOpen ? "Close Chart" : "View Graph"}
           </button>
-          <button onClick="cl1pClick" className="cl1pButton">CL1P</button>
+          <button onClick={cl1pClick} className="cl1pButton">CL1P</button>
           <select className="selectedHours" value={selectedHours} onChange={(e) => onHoursChange(Number(e.target.value))}>
             <option value={1}>1 Hour</option>
             <option value={8}>8 Hour</option>
