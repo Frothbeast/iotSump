cc4e07070a624f8b1073f01f318cda86877e6c19
frothbeast <frothbeast@gmail.com>
2026-03-19 11:43:47 -0400

    NEVER push cntrl z undo in git repo file explorer
    Signed-off-by: frothbeast <frothbeast@gmail.com>

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Start the collector in the background
-python greenhouseCollector.py &
-
-# Start the API in the foreground
-python greenhouseAPI.py
+python sumpCollector.py &
+python sumpAPI.py
